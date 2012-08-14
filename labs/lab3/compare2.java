c6cf898d94484f37a5dbf8a605643bdd8d0a1093
eastside <eastside@eastside>
2012-08-15 00:19:24 +1000

    Commit

---
+++ labs/lab3/compare2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class compare2 {
+    public static void main(String[] args){
+	/* Declare variables */
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	/* Initate Scanner */
+	Scanner scan = new Scanner(System.in);
+	/*Enter two numbers */
+	System.out.println("Please Enter a integer:");
+	num1 = scan.nextInt();
+	System.out.println("Please Enter another integer:");
+	num2 = scan.nextInt();
+	System.out.println("Please Enter another integer:");
+	num3 = scan.nextInt();
+	/* Compare them */
+	if(num1 > num2 && num2 > num3 ){
+	    System.out.println("Value in descending order:" + num1 + ", "  + num2  + ", "  + num3 );	    
+	}
+	else if (num2 > num1 && num1 > num3 ){
+	    System.out.println("Value in descending order:" + num2 +  ", " +  num1  + ", "  + num3 );	
+	}
+	else if (num3 > num2 && num2 > num1 ){
+	    System.out.println("Value in descending order:" + num3 +  ", " +  num2  + ", "  + num1 );	
+	}
+	else {
+	    System.out.println("You have entered something that is not an integer. Exiting." );	  
+	}	    
+    }
+}
